7545bdbe97e8d6f2dccfa303c5058b938d50894a
Sean Birmingham <seannbirmingham98@gmail.com>
2020-10-17 18:28:50 -0400

    setting up the state for grid and generations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Grid from './components/Grid';
 
 function App() {
+  let speed = 100;
+  let rows = 30;
+  let cols = 50;
+
+  const [generation, setGeneration] = useState(0);
+  const [fullGrid, setFullGrid] = useState(
+    Array(rows)
+      .fill()
+      .map(() => Array(cols).fill(false))
+  );
+
   return (
     <div>
       <h1>Game of Life</h1>
+      <Grid fullGrid={fullGrid} rows={rows} cols={cols} />
+      <h2>Generations: {generation}</h2>
     </div>
   );
 }
