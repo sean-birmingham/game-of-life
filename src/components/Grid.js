295f48bdb07756e849e395b1e0c89bade05ed812
Sean Birmingham <seannbirmingham98@gmail.com>
2020-10-17 18:37:33 -0400

    grid setup

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Box from './Box';
 
-const Grid = () => {
-  return <div>Grid</div>;
+const Grid = (props) => {
+  const width = props.cols * 14;
+  let rowsArray = [];
+
+  let boxClass = '';
+
+  for (let i = 0; i < props.rows; i++) {
+    for (let j = 0; j < props.cols; j++) {
+      let boxId = `${i}_${j}`;
+
+      boxClass = props.fullGrid[i][j] ? 'box on' : 'box off';
+
+      rowsArray.push(
+        <Box key={boxId} boxClass={boxClass} boxId={boxId} row={i} col={j} />
+      );
+    }
+  }
+
+  return <div className='grid' style={{ width: width }}></div>;
 };
 
 export default Grid;
